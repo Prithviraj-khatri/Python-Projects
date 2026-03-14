93bf40cd0af2c226c0bc0e68a691f5a9f40264ad
Prithviraj khatri <prithvirajkhatri2@gmail.com>
2026-03-14 21:15:35 +0530

    age

---
+++ age.py
@@ -0,0 +1,12 @@
+import time
+print("Age Checker")
+time.sleep(2)
+age = int(input("Enter your age: "))
+if age < 10:
+    print("You are a Child.")
+elif age > 10 and age < 20:
+    print("You are a Teenager.")
+elif age > 20 and age < 40:
+    print("You are an Adult.")
+else:
+    print("You are a Senior Citizen.")
