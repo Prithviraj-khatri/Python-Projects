f5a7ab2f67451e17c7f1917aec41f3bc553f3dfb
Prithviraj khatri <prithvirajkhatri2@gmail.com>
2026-03-14 21:19:58 +0530

    Gender

---
+++ gender.py
@@ -0,0 +1,8 @@
+print("Gender finder")
+gender = input("Enter your gender: ")
+if gender == "Male":
+    print("You are Male.")
+elif gender == "Female":
+    print("You are Female.")
+else:
+    print("Invalid input. Please enter Male or Female.")
