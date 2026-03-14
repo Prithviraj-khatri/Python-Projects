82178b2b12db70cfdf4b3006f5ef9f2e29578340
Prithviraj khatri <prithvirajkhatri2@gmail.com>
2026-03-14 16:21:33 +0530

    kbc game

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print ("hello world")
+print("hello welcome to the game")
+user_input = input("Enter your name: ")
+print(f"hello!{user_input} welcome to the kbc")
+question=("What is capital of india")
+print(f"{question}")
+options=["Delhi","Mumbai","Bengluru"]
+print(f"your option are {options}")
+user_answer = input("Enter your answer: ")
+if user_answer == "Delhi":
+    print("your answer is correct")
+else:
+    print("your answer is wrong") 
