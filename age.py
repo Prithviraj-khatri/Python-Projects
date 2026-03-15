d55b9ff353c35f48c89381fc467ef0a23b7d5bb5
Prithviraj khatri <prithvirajkhatri2@gmail.com>
2026-03-15 19:40:48 +0530

    evod

--- age.py
+++ age.py
@@ -2,11 +2,11 @@ import time
 print("Age Checker")
 time.sleep(2)
 age = int(input("Enter your age: "))
-if age < 10:
+if age <= 10:
     print("You are a Child.")
-elif age > 10 and age < 20:
+elif age > 10 and age <= 20:
     print("You are a Teenager.")
-elif age > 20 and age < 40:
+elif age > 20 and age <= 40:
     print("You are an Adult.")
 else:
     print("You are a Senior Citizen.")
