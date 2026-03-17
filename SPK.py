29da8dab5b1ce77f254a689728101c9b576767d6
Prithviraj khatri <prithvirajkhatri2@gmail.com>
2026-03-17 23:48:17 +0530

    SPK

---
+++ SPK.py
@@ -0,0 +1,28 @@
+import random
+computer = random.choice([0,1,2])
+user = input("enter your choice: ")
+yourDict = {"s":0,"p":1,"k":2}
+revDict = {0:"stone",1:"paper",2:"knife"}
+
+you = yourDict[user]
+
+print(f"you choose {revDict[you]}\n commputer choose {revDict[computer]}")
+
+if(computer == you):
+    print("draw")
+else:
+    if (computer == 0 and you == 1):
+        print("you won")
+    elif (computer == 0 and you == 2):
+        print("you lose")
+    elif (computer == 1 and you == 2):
+        print("you lose")
+    elif (computer == 1 and you == 0):
+        print("you won")
+    elif (computer == 2 and you == 0):
+        print("you lose")
+    elif (computer == 2 and you == 1):
+        print("you won")
+    else:
+        print("something went wrong")
+    
