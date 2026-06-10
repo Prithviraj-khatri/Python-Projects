a4b76c33eff864a36447e81e2d60c6393073e91d
Prithviraj khatri <prithvirajkhatri2@gmail.com>
2026-06-10 22:18:59 +0530

    m

---
+++ Practice_questions/3.py
@@ -0,0 +1,5 @@
+# Write a function that takes an array of numbers and returns the sum of the numbers. 
+# The numbers can be negative. 
+# If the array is empty, return 0.
+def sum_array(a):
+    return sum(a)
