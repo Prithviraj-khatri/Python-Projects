5c8ae9cfbfef049499451a936e8505fd8a9657b2
Prithviraj khatri <prithvirajkhatri2@gmail.com>
2026-06-11 18:56:39 +0530

    today

---
+++ Practice_questions/19.py
@@ -0,0 +1,16 @@
+# Write a program that finds the summation of every number from 1 to num (both inclusive). 
+# The number will always be a positive integer greater than 0. 
+# Your function only needs to return the result, what is shown between parentheses in the example below is 
+# how you reach that result and it's not part of it, see the sample tests.
+# For example (Input -> Output):
+# 2 -> 3 (1 + 2)
+# 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+def summation(num):
+    total = sum(range(1, num + 1)) # i am doing it my own rules answer over here
+    expressions = " + ".join(str(i) for i in range(1, num + 1))
+    return f"{num} --> {total} ({expressions})"
+
+num = int(input("Enter a number: "))
+print(summation(num))
+
