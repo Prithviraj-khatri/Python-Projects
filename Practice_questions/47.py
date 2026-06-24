23a00da0db95f082e8514114a6af1d1b9addb934
Prithviraj khatri <prithvirajkhatri2@gmail.com>
2026-06-24 09:42:44 +0530

    new

---
+++ Practice_questions/47.py
@@ -0,0 +1,13 @@
+'''
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+'''
+
+def sum_mix(arr):
+    total = 0
+
+    for value in arr:
+        total += int(value)
+    
+    return total
