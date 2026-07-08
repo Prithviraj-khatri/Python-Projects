ca89a134f6c0cbb6eda9f238798dcd02854878e3
Prithviraj khatri <prithvirajkhatri2@gmail.com>
2026-07-08 10:25:45 +0530

    new

--- Practice_questions/50.py
+++ Practice_questions/50.py
@@ -20,4 +20,4 @@ def two_sum(numbers,target):
         for j in range(i+1,len(numbers)):
 
             if numbers[i] + numbers[j] == target:
-                return[i,j]
+                return(i,j)
